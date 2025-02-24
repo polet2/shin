9f1d93c54ff1695f7daa0762300fdb295898e687
polet2 <1050883735@qq.com>
2025-02-24 07:01:04 -0800

    test

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main{
         "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
 
         System.out.println("Now playing the song");
-        mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
+        mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 3);
+        
+
+
         
         // --- TODO
 
